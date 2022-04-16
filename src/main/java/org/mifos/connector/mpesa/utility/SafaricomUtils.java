34c50ad38cbcd762985010e2624f30b2ce6c65a2
Ankita Ranjan <ankita@fynarfin.io>
2022-04-16 20:59:37 +0530

    addding new log

--- src/main/java/org/mifos/connector/mpesa/utility/SafaricomUtils.java
+++ src/main/java/org/mifos/connector/mpesa/utility/SafaricomUtils.java
@@ -66,9 +66,9 @@ public class SafaricomUtils {
         buyGoodsPaymentRequestDTO.setPartyA(payer);
         buyGoodsPaymentRequestDTO.setPhoneNumber(payer);
 
-        buyGoodsPaymentRequestDTO.setPartyB(Long.valueOf(mpesaProps.getTill()));
+        buyGoodsPaymentRequestDTO.setPartyB(Long.parseLong(mpesaProps.getTill()));
         logger.info("Business Shortcode" + mpesaProps.getBusinessShortCode());
-        buyGoodsPaymentRequestDTO.setBusinessShortCode(Long.valueOf(mpesaProps.getBusinessShortCode()));
+        buyGoodsPaymentRequestDTO.setBusinessShortCode(Long.parseLong(mpesaProps.getBusinessShortCode()));
 
         buyGoodsPaymentRequestDTO.setAmount(amount);
         buyGoodsPaymentRequestDTO.setPassword(getPassword(
