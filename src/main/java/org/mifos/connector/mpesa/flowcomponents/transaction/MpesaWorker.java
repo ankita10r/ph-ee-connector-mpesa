6ef1efd751bc46d9d5d2bddbcab4e563a0b74df5
Ankita Ranjan <ankita@fynarfin.io>
2022-04-21 20:50:11 +0530

    adding skip flag to skip the mpesa connector

--- src/main/java/org/mifos/connector/mpesa/flowcomponents/transaction/MpesaWorker.java
+++ src/main/java/org/mifos/connector/mpesa/flowcomponents/transaction/MpesaWorker.java
@@ -64,6 +64,7 @@ public class MpesaWorker {
                     Map<String, Object> variables = job.getVariablesAsMap();
                     mpesaUtils.setProcess(job.getBpmnProcessId());
                     if(skipMpesa){
+                        logger.info("Skipping MPESA");
                         Exchange exchange = new DefaultExchange(camelContext);
                         String serverTransactionId = exchange.getProperty(SERVER_TRANSACTION_ID, String.class);
                         variables.put(TRANSACTION_FAILED, false);
@@ -71,43 +72,43 @@ public class MpesaWorker {
                         variables.put(SERVER_TRANSACTION_ID, serverTransactionId);
                     }
                     else {
-                    TransactionChannelC2BRequestDTO channelRequest = objectMapper.readValue(
-                            (String) variables.get("mpesaChannelRequest"), TransactionChannelC2BRequestDTO .class);
-                    String transactionId = (String) variables.get(TRANSACTION_ID);
+                        TransactionChannelC2BRequestDTO channelRequest = objectMapper.readValue(
+                                (String) variables.get("mpesaChannelRequest"), TransactionChannelC2BRequestDTO.class);
+                        String transactionId = (String) variables.get(TRANSACTION_ID);
 
-                    BuyGoodsPaymentRequestDTO buyGoodsPaymentRequestDTO = safaricomUtils.channelRequestConvertor(
-                            channelRequest);
-                    logger.info(buyGoodsPaymentRequestDTO.toString());
-                    Exchange exchange = new DefaultExchange(camelContext);
-                    exchange.setProperty(BUY_GOODS_REQUEST_BODY, buyGoodsPaymentRequestDTO);
-                    exchange.setProperty(CORRELATION_ID, transactionId);
-                    exchange.setProperty(DEPLOYED_PROCESS,job.getBpmnProcessId());
+                        BuyGoodsPaymentRequestDTO buyGoodsPaymentRequestDTO = safaricomUtils.channelRequestConvertor(
+                                channelRequest);
+                        logger.info(buyGoodsPaymentRequestDTO.toString());
+                        Exchange exchange = new DefaultExchange(camelContext);
+                        exchange.setProperty(BUY_GOODS_REQUEST_BODY, buyGoodsPaymentRequestDTO);
+                        exchange.setProperty(CORRELATION_ID, transactionId);
+                        exchange.setProperty(DEPLOYED_PROCESS, job.getBpmnProcessId());
 
-                    variables.put(BUY_GOODS_REQUEST_BODY, buyGoodsPaymentRequestDTO);
+                        variables.put(BUY_GOODS_REQUEST_BODY, buyGoodsPaymentRequestDTO);
 
-                    producerTemplate.send("direct:buy-goods-base", exchange);
+                        producerTemplate.send("direct:buy-goods-base", exchange);
 
-                    boolean isTransactionFailed = exchange.getProperty(TRANSACTION_FAILED, boolean.class);
-                    if(isTransactionFailed) {
-                        variables.put(TRANSACTION_FAILED, true);
-                        variables.put(TRANSFER_CREATE_FAILED, true);
-                        variables.put(TRANSFER_RESPONSE_CREATE, ZeebeUtils.getTransferResponseCreateJson());
-                        String errorBody = exchange.getProperty(ERROR_INFORMATION, String.class);
-                        variables.put(ERROR_INFORMATION, errorBody);
-                        variables.put(ERROR_CODE, exchange.getProperty(ERROR_CODE, String.class));
-                        variables.put(ERROR_DESCRIPTION, exchange.getProperty(ERROR_DESCRIPTION, String.class));
-                    } else {
-                        String serverTransactionId = exchange.getProperty(SERVER_TRANSACTION_ID, String.class);
-                        variables.put(TRANSACTION_FAILED, false);
-                        variables.put(TRANSFER_CREATE_FAILED, false);
-                        variables.put(SERVER_TRANSACTION_ID, serverTransactionId);
+                        boolean isTransactionFailed = exchange.getProperty(TRANSACTION_FAILED, boolean.class);
+                        if (isTransactionFailed) {
+                            variables.put(TRANSACTION_FAILED, true);
+                            variables.put(TRANSFER_CREATE_FAILED, true);
+                            variables.put(TRANSFER_RESPONSE_CREATE, ZeebeUtils.getTransferResponseCreateJson());
+                            String errorBody = exchange.getProperty(ERROR_INFORMATION, String.class);
+                            variables.put(ERROR_INFORMATION, errorBody);
+                            variables.put(ERROR_CODE, exchange.getProperty(ERROR_CODE, String.class));
+                            variables.put(ERROR_DESCRIPTION, exchange.getProperty(ERROR_DESCRIPTION, String.class));
+                        } else {
+                            String serverTransactionId = exchange.getProperty(SERVER_TRANSACTION_ID, String.class);
+                            variables.put(TRANSACTION_FAILED, false);
+                            variables.put(TRANSFER_CREATE_FAILED, false);
+                            variables.put(SERVER_TRANSACTION_ID, serverTransactionId);
+                        }
                     }
 
                     client.newCompleteCommand(job.getKey())
                             .variables(variables)
                             .send()
                             .join();
-                }
                 })
                 .name("init-transfer")
                 .maxJobsActive(workerMaxJobs)
